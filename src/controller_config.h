68c015f61866191926e28fd5bf0bca2f543fe195
GHN <gbfdum01@gmail.com>
2025-09-27 14:12:26 +0800

    test

--- src/controller_config.h
+++ src/controller_config.h
@@ -1,12 +1,12 @@
 #ifndef CONTROLLER_CONFIG_H
 #define CONTROLLER_CONFIG_H
 
-#define SW_GPIO_SIZE 8               // Number of switches
-#define LED_GPIO_SIZE 0              // Number of switch LEDs
+#define SW_GPIO_SIZE 9                // Number of switches
+#define LED_GPIO_SIZE 1               // Number of switch LEDs
 #define ENC_GPIO_SIZE 2               // Number of encoders
-#define ENC_PPR 24                   // Encoder PPR
+#define ENC_PPR 24                    // Encoder PPR
 #define MOUSE_SENS 5                  // Mouse sensitivity multiplier
-#define ENC_DEBOUNCE true            // Encoder Debouncing
+#define ENC_DEBOUNCE true             // Encoder Debouncing
 #define SW_DEBOUNCE_TIME_US 4000      // Switch debounce delay in us
 #define ENC_PULSE (ENC_PPR * 4)       // 4 pulses per PPR
 #define REACTIVE_TIMEOUT_MAX 1000000  // HID to reactive timeout in us
