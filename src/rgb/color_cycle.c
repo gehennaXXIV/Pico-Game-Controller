fc1538c438a52a610ae82be064477f1581e76ba4
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-07 22:20:08 +0800

    rgb fix

--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -1,40 +1,56 @@
-// 1. We create a "memory" for the brightness of each of the 9 LEDs.
-// We use 'static' so the Pico remembers these numbers even after the function finishes.
+// Static memory so the Pico remembers the brightness levels between loops
 static uint8_t brightness[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 void ws2812b_color_cycle(uint32_t counter) {
-    // Your Button to LED mapping from before
+    // 1. THE MAP: Button index -> Physical LED index
+    // Button 0 (Pin 15) -> LED 8
+    // Button 1 (Pin 14) -> LED 0
+    // ...etc
     int led_map[9] = {8, 0, 7, 1, 6, 2, 5, 3, 4};
 
-    // The base colors you want
-    // Note: These are R, G, B values. We will apply brightness to these.
-    uint8_t colors_r[9] = {255, 255, 0,   0,   255, 0,   0,   255, 255};
-    uint8_t colors_g[9] = {255, 255, 255, 0,   0,   0,   255, 255, 255};
-    uint8_t colors_b[9] = {255, 0,   0,   255, 0,   255, 0,   0,   255};
+    // 2. THE COLORS (Defined by Button Index 0-8)
+    // These are the R, G, B values for your requested sequence:
+    // White, Yellow, Green, Blue, Red, Blue, Green, Yellow, White
+    uint8_t btn_r[9] = {255, 255, 0,   0,   255, 0,   0,   255, 255};
+    uint8_t btn_g[9] = {255, 255, 255, 0,   0,   0,   255, 255, 255};
+    uint8_t btn_b[9] = {255, 0,   0,   255, 0,   255, 0,   0,   255};
 
-    // 2. Check Buttons
+    // 3. LOGIC: Check buttons and update brightness
     for (int i = 0; i < 9; i++) {
+        int physical_led = led_map[i];
+
         if (!gpio_get(SW_GPIO[i])) { 
-            // Button is pressed: Set brightness to MAX
-            brightness[led_map[i]] = 255;
+            // Button pressed: Jump to max brightness
+            brightness[physical_led] = 255;
         } else {
-            // Button is NOT pressed: Slowly fade out
-            // To make it fade SLOWER, change '2' to '1'. 
-            // To make it fade FASTER, change '2' to '5'.
-            if (brightness[led_map[i]] > 0) {
-                brightness[led_map[i]] -= 1; 
+            // Button released: Fade out slowly (~1 second)
+            if (brightness[physical_led] > 0) {
+                brightness[physical_led] -= 1; 
             }
         }
     }
 
-    // 3. Send to LEDs
+    // 4. OUTPUT: Calculate final colors and send to the chain
+    // We loop through the physical LEDs (0-8) to send the pixels in order
     for (int i = 0; i < 9; i++) {
-        // We calculate the color based on the current brightness
-        // This math scales the 255 color down to 0
-        uint8_t r = (colors_r[i] * brightness[i]) / 255;
-        uint8_t g = (colors_g[i] * brightness[i]) / 255;
-        uint8_t b = (colors_b[i] * brightness[i]) / 255;
-        
-        put_pixel(urgb_u32(r, g, b));
+        // We need to find which button controls this physical LED to get its color
+        int button_index = -1;
+        for(int j=0; j<9; j++) {
+            if(led_map[j] == i) {
+                button_index = j;
+                break;
+            }
+        }
+
+        if (button_index != -1) {
+            // Apply the brightness fade to the specific button's color
+            uint8_t r = (btn_r[button_index] * brightness[i]) / 255;
+            uint8_t g = (btn_g[button_index] * brightness[i]) / 255;
+            uint8_t b = (btn_b[button_index] * brightness[i]) / 255;
+            
+            put_pixel(urgb_u32(r, g, b));
+        } else {
+            put_pixel(0);
+        }
     }
 }
