88ef56c1b7f5da242a84396980dc57e5c2a755b0
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-07 21:45:08 +0800

    v1.2

--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -1,5 +1,5 @@
 void ws2812b_color_cycle(uint32_t counter) {
-    // Your 9 colors in order
+    // 1. Your requested colors
     uint32_t colors[9] = {
         urgb_u32(255, 255, 255), // White
         urgb_u32(255, 255, 0),   // Yellow
@@ -12,12 +12,24 @@ void ws2812b_color_cycle(uint32_t counter) {
         urgb_u32(255, 255, 255)  // White
     };
 
+    // 2. THE LED MAP
+    // This tells the code: Button 0 controls LED 8, Button 1 controls LED 0, etc.
+    // (Note: We use 0-8 for coding instead of 1-9)
+    int led_map[9] = {8, 0, 7, 1, 6, 2, 5, 3, 4};
+
+    // 3. Create a temporary list to hold which LEDs should be ON
+    uint32_t led_output[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
     for (int i = 0; i < 9; i++) {
-        // We use !gpio_get because buttons are 0 when pressed
+        // If button 'i' is pressed...
         if (!gpio_get(SW_GPIO[i])) { 
-            put_pixel(colors[i]);
-        } else {
-            put_pixel(0); // Off when not pressed
+            // ...put that button's color into the correct LED position
+            led_output[led_map[i]] = colors[i];
         }
+    }
+
+    // 4. Send the pixels to the chain in the physical order (0 to 8)
+    for (int i = 0; i < 9; i++) {
+        put_pixel(led_output[i]);
     }
 }
