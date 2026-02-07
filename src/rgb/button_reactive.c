a0aea4da901f82501170343eff67f00eb7bc9bb2
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-07 21:13:21 +0800

    Update button_reactive.c

--- src/rgb/button_reactive.c
+++ src/rgb/button_reactive.c
@@ -1,3 +1,6 @@
+#include "../controller_config.h"  // <--- ADD THIS LINE
+#include "pico/stdlib.h"           // Adds the gpio_get tool
+
 /**
  * Button Reactive Lighting for 9 LEDs
  **/
@@ -10,16 +13,14 @@
 #define COLOR_OFF    urgb_u32(0, 0, 0)
 
 void button_reactive_lighting(uint32_t unused) {
-    // Your requested color sequence
     uint32_t target_colors[9] = {
         COLOR_WHITE, COLOR_YELLOW, COLOR_GREEN, COLOR_BLUE, 
         COLOR_RED, 
         COLOR_BLUE, COLOR_GREEN, COLOR_YELLOW, COLOR_WHITE
     };
 
-    // Check each of the 9 buttons
     for (int i = 0; i < 9; i++) {
-        // gpio_get reads the pins defined in controller_config.h
+        // Now BUTTON_GPIO will be recognized
         if (!gpio_get(BUTTON_GPIO[i])) { 
             put_pixel(target_colors[i]);
         } else {
