f4aa381045108592cba2d458f6034e87a2516040
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-07 19:29:40 +0800

    1.1

--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -1,11 +1,7 @@
 void ws2812b_color_cycle(uint32_t counter) {
     (void)counter;
 
-    const uint8_t BUTTON_PINS[9] = {
-        15, 14, 12, 13, 11, 9, 10, 8, 7
-    };
-
-    const RGB_t BUTTON_COLORS[9] = {
+    const RGB_t COLORS[9] = {
         {255, 255, 255}, // white
         {255, 255,   0}, // yellow
         {  0, 255,   0}, // green
@@ -17,24 +13,14 @@ void ws2812b_color_cycle(uint32_t counter) {
         {255, 255, 255}  // white
     };
 
-    // clear LEDs
     for (int i = 0; i < WS2812B_LED_SIZE; i++) {
-        ws2812b_data[i] = (RGB_t){0, 0, 0};
-    }
+        RGB_t c = {0, 0, 0};
 
-    // light LED only when its button is pressed
-    for (int i = 0; i < 9; i++) {
-        if (!gpio_get(BUTTON_PINS[i])) { // active-low
-            ws2812b_data[i] = BUTTON_COLORS[i];
+        // buttons 0–8 map to LEDs 0–8
+        if (i < 9 && ((report.buttons >> i) & 1)) {
+            c = COLORS[i];
         }
-    }
 
-    // push data
-    for (int i = 0; i < WS2812B_LED_SIZE; i++) {
-        put_pixel(urgb_u32(
-            ws2812b_data[i].r,
-            ws2812b_data[i].g,
-            ws2812b_data[i].b
-        ));
+        put_pixel(urgb_u32(c.r, c.g, c.b));
     }
 }
