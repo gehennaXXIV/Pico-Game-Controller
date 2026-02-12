800654c2345bd609e2a34c0663eed57c926abdba
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-12 21:15:26 +0800

    rgb v1.1

--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -8,53 +8,48 @@ void ws2812b_color_cycle(uint32_t counter) {
     uint8_t MAX_BRIGHTNESS = 100; 
     int led_map[9] = {8, 0, 7, 1, 6, 2, 5, 3, 4};
 
+    // Button colors (RGB)
     uint8_t btn_r[9] = {255, 255, 0,   0,   255, 0,   0,   255, 255};
     uint8_t btn_g[9] = {255, 255, 255, 0,   0,   0,   255, 255, 255};
     uint8_t btn_b[9] = {255, 0,   0,   255, 0,   255, 0,   0,   255};
 
     bool activity = false;
 
-    // 1. Check Physical Buttons
+    // 1. Check Buttons
     for (int i = 0; i < 9; i++) {
         int physical_led = led_map[i];
         if (!gpio_get(SW_GPIO[i])) { 
             brightness[physical_led] = MAX_BRIGHTNESS;
             activity = true;
         } else {
-            // Constant Fade logic for all LEDs
             if (brightness[physical_led] > 0) {
                 brightness[physical_led] -= 1; 
             }
         }
     }
 
-    // 2. Idle Logic
+    // 2. Idle Timer Logic
     if (activity) {
         idle_counter = 0;
         animation_timer = 0;
-        // Optional: Reset scanner to start position for next time
         scanner_pos = 0;
         scanner_dir = 1;
     } else {
         idle_counter++; 
     }
 
-    // 3. Trigger Animation after 5 seconds (1000 loops)
-    if (idle_counter > 1000) {
+    // 3. Animation Trigger (2000 loops = 10 seconds)
+    if (idle_counter > 2000) {
         animation_timer++;
         
-        // Speed of the "bounce": move every 100ms (20 loops)
-        // Change 20 to a higher number to make the animation slower
+        // Move every 20 loops (100ms)
         if (animation_timer >= 20) {
             animation_timer = 0;
             
-            // "Light up" the current position in the sequence
             brightness[led_map[scanner_pos]] = MAX_BRIGHTNESS;
 
-            // Step the scanner
             scanner_pos += scanner_dir;
 
-            // Bounce logic
             if (scanner_pos >= 8) {
                 scanner_pos = 8;
                 scanner_dir = -1;
@@ -65,7 +60,7 @@ void ws2812b_color_cycle(uint32_t counter) {
         }
     }
 
-    // 4. Send to LED Strip
+    // 4. Output to LED Strip
     for (int i = 0; i < 9; i++) {
         int button_index = -1;
         for(int j = 0; j < 9; j++) {
@@ -76,4 +71,4 @@ void ws2812b_color_cycle(uint32_t counter) {
         }
 
         if (button_index != -1) {
-            uint8_t r = (btn_r[button_index] * brightness[i]) / 255;
+            uint8_t r = (btn_r[button_index] * brightness[i]) / 25
