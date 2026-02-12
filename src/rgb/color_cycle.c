fff77b03d8c6aca01b0f0629d8d1b7c0b3d0053f
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-12 21:20:11 +0800

    v1.1 rgb

--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -1,3 +1,12 @@
+#include "rgb_include.h"
+
+// Helper function to keep numbers between 0 and 255
+int i_clamp(int d, int min, int max) {
+  const int t = d < min ? min : d;
+  return t > max ? max : t;
+}
+
+// Memory for the fade and idle animation
 static uint8_t brightness[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 static uint32_t idle_counter = 0;
 static int scanner_pos = 0;
@@ -15,7 +24,7 @@ void ws2812b_color_cycle(uint32_t counter) {
 
     bool activity = false;
 
-    // 1. Check Buttons
+    // 1. Check Buttons & Handle Fade
     for (int i = 0; i < 9; i++) {
         int physical_led = led_map[i];
         if (!gpio_get(SW_GPIO[i])) { 
@@ -41,15 +50,10 @@ void ws2812b_color_cycle(uint32_t counter) {
     // 3. Animation Trigger (2000 loops = 10 seconds)
     if (idle_counter > 2000) {
         animation_timer++;
-        
-        // Move every 20 loops (100ms)
         if (animation_timer >= 20) {
             animation_timer = 0;
-            
             brightness[led_map[scanner_pos]] = MAX_BRIGHTNESS;
-
             scanner_pos += scanner_dir;
-
             if (scanner_pos >= 8) {
                 scanner_pos = 8;
                 scanner_dir = -1;
@@ -71,4 +75,12 @@ void ws2812b_color_cycle(uint32_t counter) {
         }
 
         if (button_index != -1) {
-            uint8_t r = (btn_r[button_index] * brightness[i]) / 25
+            uint8_t r = i_clamp((btn_r[button_index] * brightness[i]) / 255, 0, 255);
+            uint8_t g = i_clamp((btn_g[button_index] * brightness[i]) / 255, 0, 255);
+            uint8_t b = i_clamp((btn_b[button_index] * brightness[i]) / 255, 0, 255);
+            put_pixel(urgb_u32(r, g, b));
+        } else {
+            put_pixel(0);
+        }
+    }
+}
