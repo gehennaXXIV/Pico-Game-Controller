7adfb3e7180418fbe5f27fa47b08d9525c22415b
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-02-17 11:54:25 +0800

    v1.4

--- src/pico_game_controller.c
+++ src/pico_game_controller.c
@@ -1,171 +1,91 @@
-/*
- * Pico Game Controller v1.4
- */
 #define PICO_GAME_CONTROLLER_C
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "bsp/board.h"
-#include "controller_config.h"
-#include "encoders.pio.h"
-#include "hardware/clocks.h"
-#include "hardware/dma.h"
-#include "hardware/irq.h"
-#include "hardware/pio.h"
-#include "pico/multicore.h"
 #include "pico/stdlib.h"
+#include "controller_config.h"
+#include "pico/multicore.h"
 #include "tusb.h"
 #include "usb_descriptors.h"
-
-// clang-format off
 #include "debounce/debounce_include.h"
 #include "rgb/rgb_include.h"
-// clang-format on
 
-// --- GLOBAL VARIABLES ---
+// Global Brightness
 uint8_t global_brightness = 50; 
 
-PIO pio, pio_1;
-uint32_t enc_val[ENC_GPIO_SIZE];
-uint32_t prev_enc_val[ENC_GPIO_SIZE];
-int cur_enc_val[ENC_GPIO_SIZE];
-
-bool sw_prev_raw_val[SW_GPIO_SIZE];
-bool sw_cooked_val[SW_GPIO_SIZE];
-uint64_t sw_timestamp[SW_GPIO_SIZE];
-
-bool kbm_report;
-uint64_t reactive_timeout_timestamp;
-
-void (*ws2812b_mode)();
-void (*loop_mode)();
-void (*debounce_mode)();
-bool joy_mode_check = true;
-
-union {
-  struct {
-    uint8_t buttons[LED_GPIO_SIZE];
-    RGB_t rgb[WS2812B_LED_ZONES];
-  } lights;
-  uint8_t raw[LED_GPIO_SIZE + WS2812B_LED_ZONES * 3];
-} lights_report;
-
+// Controller State
 struct report {
   uint16_t buttons;
   uint8_t joy0;
   uint8_t joy1;
 } report;
 
-void ws2812b_update(uint32_t counter) {
-    ws2812b_mode(counter); 
-}
+bool sw_cooked_val[SW_GPIO_SIZE];
+void (*loop_mode)();
+void (*ws2812b_mode)();
+void (*debounce_mode)();
 
+// Gamepad Detection Fix
 void joy_mode() {
   if (tud_hid_ready()) {
-    report.joy0 = 0;
-    report.joy1 = 0;
+    report.joy0 = 127; // Center stick
+    report.joy1 = 127;
     tud_hid_n_report(0x00, REPORT_ID_JOYSTICK, &report, sizeof(report));
   }
 }
 
+// Keyboard Mode
 void key_mode() {
   if (tud_hid_ready()) {
-    uint8_t nkro_report[32] = {0};
+    uint8_t nkro[32] = {0};
     for (int i = 0; i < SW_GPIO_SIZE; i++) {
       if ((report.buttons >> i) & 1) {
         uint8_t bit = SW_KEYCODE[i] % 8;
         uint8_t byte = (SW_KEYCODE[i] / 8) + 1;
-        if (SW_KEYCODE[i] >= 240 && SW_KEYCODE[i] <= 247) {
-          nkro_report[0] |= (1 << bit);
-        } else if (byte > 0 && byte <= 31) {
-          nkro_report[byte] |= (1 << bit);
-        }
+        if (SW_KEYCODE[i] >= 240 && SW_KEYCODE[i] <= 247) nkro[0] |= (1 << bit);
+        else if (byte > 0 && byte <= 31) nkro[byte] |= (1 << bit);
       }
     }
-    tud_hid_n_report(0x00, REPORT_ID_KEYBOARD, &nkro_report, sizeof(nkro_report));
+    tud_hid_n_report(0x00, REPORT_ID_KEYBOARD, &nkro, sizeof(nkro));
   }
 }
 
+// Fixed Update Inputs (No Freezing)
 void update_inputs() {
   report.buttons = 0;
   for (int i = SW_GPIO_SIZE - 1; i >= 0; i--) {
-    sw_prev_raw_val[i] = !gpio_get(SW_GPIO[i]);
     report.buttons <<= 1;
     report.buttons |= sw_cooked_val[i];
   }
 
-  // Brightness Control (Hold 11, Press 2 or 7)
+  // Brightness: Hold Button 11 (Index 10)
   if (!gpio_get(SW_GPIO[10])) {
-    if (!gpio_get(SW_GPIO[1])) {
-      if (global_brightness > 5) global_brightness -= 5;
-      sleep_ms(50);
+    static uint32_t last_tick = 0;
+    if (board_millis() - last_tick > 50) { // Non-blocking timer
+      if (!gpio_get(SW_GPIO[1]) && global_brightness > 5) global_brightness -= 5;
+      if (!gpio_get(SW_GPIO[6]) && global_brightness < 250) global_brightness += 5;
+      last_tick = board_millis();
     }
-    if (!gpio_get(SW_GPIO[6])) {
-      if (global_brightness < 250) global_brightness += 5;
-      sleep_ms(50);
-    }
-    report.buttons &= ~(1 << 1); 
-    report.buttons &= ~(1 << 6);
+    report.buttons &= ~((1 << 1) | (1 << 6)); // Mask keys 2 and 7
   }
-}
-
-void update_lights() {
-  for (int i = 0; i < LED_GPIO_SIZE - 1; i++) {
-    if (!gpio_get(SW_GPIO[i])) {
-      gpio_put(LED_GPIO[i], 1);
-    } else {
-      gpio_put(LED_GPIO[i], 0);
-    }
-  }
-}
-
-void dma_handler() {
-  uint i = 1;
-  int interrupt_channel = 0;
-  while ((i & dma_hw->ints0) == 0) {
-    i = i << 1;
-    ++interrupt_channel;
-  }
-  dma_hw->ints0 = 1u << interrupt_channel;
 }
 
 void core1_entry() {
   uint32_t counter = 0;
   while (1) {
-    ws2812b_update(++counter);
+    ws2812b_color_cycle(++counter);
     sleep_ms(5);
   }
 }
 
 void init() {
-  gpio_init(25);
-  gpio_set_dir(25, GPIO_OUT);
-  gpio_put(25, 1);
-
-  pio_1 = pio1;
-  uint offset2 = pio_add_program(pio_1, &ws2812_program);
-  ws2812_program_init(pio_1, 0, offset2, WS2812B_GPIO, 800000, false);
-
   for (int i = 0; i < SW_GPIO_SIZE; i++) {
     gpio_init(SW_GPIO[i]);
     gpio_set_dir(SW_GPIO[i], GPIO_IN);
     gpio_pull_up(SW_GPIO[i]);
   }
-
-  for (int i = 0; i < LED_GPIO_SIZE; i++) {
-    gpio_init(LED_GPIO[i]);
-    gpio_set_dir(LED_GPIO[i], GPIO_OUT);
-  }
-
-  if (!gpio_get(SW_GPIO[10])) {
-    loop_mode = &key_mode;
-  } else {
-    loop_mode = &joy_mode;
-  }
-
-  ws2812b_mode = &ws2812b_color_cycle;
+  // Default logic: hold button 11 at boot for Keyboard, otherwise Gamepad
+  if (!gpio_get(SW_GPIO[10])) loop_mode = &key_mode;
+  else loop_mode = &joy_mode;
+  
   debounce_mode = &debounce_eager;
   multicore_launch_core1(core1_entry);
 }
@@ -179,14 +99,10 @@ int main(void) {
     debounce_mode();
     update_inputs();
     loop_mode();
-    update_lights();
   }
   return 0;
 }
 
-uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
-  return 0;
-}
-
-void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
-}
+// Mandatory Callbacks
+uint16_t tud_hid_get_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) { return 0; }
+void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {}
--- src/rgb/color_cycle.c
+++ src/rgb/color_cycle.c
@@ -1,33 +1,25 @@
 static uint8_t brightness[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-static uint32_t idle_timer = 1001; 
+static uint32_t idle_timer = 1001;
 static int scan_idx = 0;
 static int scan_dir = 1;
 
-extern uint8_t global_brightness; 
+extern uint8_t global_brightness; // Link to the variable
 
 void ws2812b_color_cycle(uint32_t counter) {
-    uint8_t MAX_BRIGHTNESS = global_brightness;  
-    int animation_speed = 60;     
+    uint8_t MAX_BRIGHTNESS = global_brightness; // Use the global variable
+    int animation_speed = 60;
     int led_map[9] = {8, 0, 7, 1, 6, 2, 5, 3, 4};
-
-    uint8_t btn_r[9] = {255, 255, 0,   0,   255, 0,   0,   255, 255};
-    uint8_t btn_g[9] = {255, 255, 255, 0,   0,   0,   255, 255, 255};
-    uint8_t btn_b[9] = {255, 0,   0,   255, 0,   255, 0,   0,   255};
+    uint8_t btn_r[9] = {255, 255, 0, 0, 255, 0, 0, 255, 255};
+    uint8_t btn_g[9] = {255, 255, 255, 0, 0, 0, 255, 255, 255};
+    uint8_t btn_b[9] = {255, 0, 0, 255, 0, 255, 0, 0, 255};
 
     bool active = false;
-
     for (int i = 0; i < 9; i++) {
-        int physical_led = led_map[i];
         if (!gpio_get(SW_GPIO[i])) { 
-            brightness[physical_led] = MAX_BRIGHTNESS;
+            brightness[led_map[i]] = MAX_BRIGHTNESS;
             active = true;
-        } else {
-            if (brightness[physical_led] > MAX_BRIGHTNESS) {
-                brightness[physical_led] = MAX_BRIGHTNESS;
-            }
-            if (brightness[physical_led] > 0) {
-                brightness[physical_led] -= 1; 
-            }
+        } else if (brightness[led_map[i]] > 0) {
+            brightness[led_map[i]] -= 1; 
         }
     }
 
@@ -35,25 +27,18 @@ void ws2812b_color_cycle(uint32_t counter) {
         idle_timer = 0;
     } else {
         idle_timer++;
-        if (idle_timer > 1000) {
-            if (idle_timer % animation_speed == 0) {
-                brightness[led_map[scan_idx]] = MAX_BRIGHTNESS;
-                brightness[led_map[8 - scan_idx]] = MAX_BRIGHTNESS;
-                scan_idx += scan_dir;
-                if (scan_idx >= 4) { scan_idx = 4; scan_dir = -1; }
-                else if (scan_idx <= 0) { scan_idx = 0; scan_dir = 1; }
-            }
+        if (idle_timer > 1000 && idle_timer % animation_speed == 0) {
+            brightness[led_map[scan_idx]] = MAX_BRIGHTNESS;
+            brightness[led_map[8 - scan_idx]] = MAX_BRIGHTNESS;
+            scan_idx += scan_dir;
+            if (scan_idx >= 4) { scan_idx = 4; scan_dir = -1; }
+            else if (scan_idx <= 0) { scan_idx = 0; scan_dir = 1; }
         }
     }
 
     for (int i = 0; i < 9; i++) {
         int button_index = -1;
-        for(int j = 0; j < 9; j++) {
-            if(led_map[j] == i) {
-                button_index = j;
-                break;
-            }
-        }
+        for(int j = 0; j < 9; j++) if(led_map[j] == i) { button_index = j; break; }
         if (button_index != -1) {
             uint8_t r = (btn_r[button_index] * brightness[i]) / 255;
             uint8_t g = (btn_g[button_index] * brightness[i]) / 255;
