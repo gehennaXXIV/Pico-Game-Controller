efc99641029f4cfabdfa1896662eaf54e63eef67
GHN <122549124+gehennaXXIV@users.noreply.github.com>
2026-04-25 22:42:47 +0800

    v1

--- src/pico_game_controller.c
+++ src/pico_game_controller.c
@@ -271,9 +271,9 @@ void init() {
 
   // RGB Mode Switching
   if (!gpio_get(SW_GPIO[1])) {
-    ws2812b_mode = &turbocharger_color_cycle;
+      ws2812b_mode = &turbocharger_color_cycle;  // hold button 2 at boot
   } else {
-    ws2812b_mode = &ws2812b_color_cycle;
+      ws2812b_mode = &ghn;  // ← was &ws2812b_color_cycle
   }
 
   // Debouncing Mode
